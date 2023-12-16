5b22daca6dc32328d9c2e1c2033690e082778410
hagermatarawy <siydahmed8@gmail.com>
2023-12-16 18:55:17 +0200

    add file

---
+++ first.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n1, n2;
+    cout<<"enter first number";
+    cin>>n1;
+    cout<<"enter second number";
+    cin>>n2;
+    cout<<n1+n2;
+
+    
+    
+    cout<<"github";
+    return 0;
+}
